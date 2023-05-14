5ab829bb2fc76be08f81526f336f97ab82d33cc3
roman <romanze1706@gmail.com>
2023-05-14 15:07:23 +0300

    Подключены необходимые библиотеки

---
+++ src/main/java/com/example/minrecbotjava/MinRecBotJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.minrecbotjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinRecBotJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinRecBotJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minrecbotjava/MinRecBotJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minrecbotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinRecBotJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
